ee31bcf3523799a2a662d16c0234400f992ad58a
andfaulkner <andfaulkner@gmail.com>
2017-02-16 06:03:01 -0500

    convenience module for adding server styles. Accessible via mad-logs/server-styles. Fixed typing annoyance with lodash.

---
+++ lib/server-styles.d.ts
@@ -0,0 +1,18 @@
+import * as colors from 'colors';
+/****
+ *
+ * Taken from colors - this is a collection of styles that are nicely visible on the terminal.
+ * It was a royal pain in the ass determining which were good and which were invisible.
+ *
+ */
+export declare const serverStyles: {
+    blueBgWhite: colors.Color;
+    magentaBgWhite: colors.Color;
+    cyanBgBlack: colors.Color;
+    whiteBgYellow: colors.Color;
+    whiteBgBlack: colors.Color;
+    blackBgCyanItalic: colors.Color;
+    blackBgGreen: colors.Color;
+    blackBgYellow: colors.Color;
+    blackBgWhite: colors.Color;
+};
