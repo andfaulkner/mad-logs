42bb32113303f3c7d6eba03b3b12437a27b8fcb8
Andrew Faulkner <andfaulkner@gmail.com>
2018-07-06 01:55:29 -0400

    Doc format fixes in REPL

--- mad-logs-repl.ts
+++ mad-logs-repl.ts
@@ -25,14 +25,14 @@ const packageJson = require('./package.json');
 const {defineProperty} = Object;
 
 export const defPropConfig = {
-    // Configuration for a global value that cannot be reassigned in the repl.
+    // Configuration for a global value that cannot be reassigned in the repl
     immutable: (lib: any) => ({
         configurable: false,
         enumerable: true,
         writable: false,
         value: lib,
     }),
-    // Configuration for a global value that can be reassigned in the repl.
+    // Configuration for a global value that can be reassigned in the repl
     mutable: (lib: any) => ({
         configurable: true,
         enumerable: true,
@@ -42,7 +42,7 @@ export const defPropConfig = {
 };
 
 /**
- * Run when inspect is called in the repl.
+ * Run when inspect is called in the repl
  */
 export const inspect = (...args) => {
     (console.log as any)(
@@ -61,37 +61,37 @@ export const r = repl.start({useColors: true});
 const historyFile = path.join(rootPath, '.node_history');
 require('repl.history')(r, historyFile);
 
-// Add IN_REPL property to repl environment. Acts as identifier that REPL is currently running.
+// Add IN_REPL property to repl environment. Acts as identifier that REPL is currently running
 defineProperty(r.context.process.env, 'IN_REPL', defPropConfig.immutable(true));
 
 /****************************************** REPL HELPERS ******************************************/
 /**
- * Bind given properties to the repl context, with the given values.
- * Display as list on repl load, with descriptions for each specified in descriptions prop.
- * @param {Object} activeRepl Started repl (through repl.start()).
- * @param {Object} ctxProps Bind each given value to its corresponding key.
- *                 e.g. { _: lodash, _m: madUtils, Promise: bluebird }
- * @param {Object} descriptions Optional matching descriptions to display beside prop w/ given key.
- *                 e.g.: { _: 'lodash alias', bluebird: 'promises library' }
+ * Bind given properties to the repl context, with the given values
+ * Display as list on repl load, with descriptions for each specified in descriptions prop
+ * @param {Object} activeRepl Started repl (through repl.start())
+ * @param {Object} ctxProps Bind each given value to its corresponding key
+ *                 e.g. {_: lodash, _m: madUtils, Promise: bluebird}
+ * @param {Object} descriptions Optional matching descriptions to display beside prop w/ given key
+ *                 e.g.: {_: 'lodash alias', bluebird: 'promises library'}
  * @example bindPropsToRepl(repl.start(), { _: lodash, projData }, { _: 'Util lib' });
  */
 export const bindPropsToRepl = (ctxProps: Object, descriptions: {[key: string]: string}) => {
     console.log(`\n\nWelcome to the Javelinscript REPL!`);
     console.log(`Custom properties bound to the top-level context:`);
 
-    // Iterate through the given context properties.
+    // Iterate through the given context properties
     for (let [key, val] of lodash.toPairs(ctxProps)) {
-        // Add current prop's value to repl context. Mutable if requested, immutable otherwise.
+        // Add current prop's value to repl context. Mutable if requested, immutable otherwise
         if (typeof val === 'object' && val.val && val.mutable) {
             defineProperty(r.context, key, defPropConfig.mutable(val.val));
         } else {
             defineProperty(r.context, key, defPropConfig.immutable(val));
         }
 
-        // Display prop and (if defined) prop description on repl boot.
+        // Display prop and (if defined) prop description on repl boot
         if (descriptions[key]) {
             console.log(` * ${key}: ${descriptions[key]}`);
-            // If prop description provided, bind it to the object in the context.
+            // If prop description provided, bind it to the object in the context
             defineProperty(val, `__repl_description__`, defPropConfig.immutable(descriptions[key]));
         } else {
             console.log(` * ${key}`);
@@ -102,7 +102,7 @@ export const bindPropsToRepl = (ctxProps: Object, descriptions: {[key: string]: 
 
 /***************************************** PROPS TO BIND ******************************************/
 /**
- * Properties to bind to repl context (available at top level in repl).
+ * Properties to bind to repl context (available at top level in repl)
  */
 const ctxProps = {
     // Helper libraries
@@ -130,7 +130,7 @@ const ctxProps = {
 };
 
 /**
- * Extra descriptions for bound properties.
+ * Extra descriptions for bound properties
  */
 const descriptions = {
     _: 'lodash alias',
