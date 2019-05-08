552e9d092b7292520068c2588c13dfaad0234938
Andrew Faulkner <andfaulkner@gmail.com>
2019-05-08 06:56:38 -0400

    Isomorphic styles fixed (probably)

--- src/isomorphic-styling.ts
+++ src/isomorphic-styling.ts
@@ -1,3 +1,5 @@
+import isNode from 'detect-node';
+
 const cssInverse = `filter: invert(100%); -moz-filter: invert(100%); -webkit-filter: invert(100%);`;
 
 // prettier-ignore
@@ -5,42 +7,39 @@ const cssInverse = `filter: invert(100%); -moz-filter: invert(100%); -webkit-fil
  * Collection of atomic styles that can be used in both the browser and the cli
  */
 export const isoStyling = {
-    default: {
-        cli:     `\u001b[49m\u001b[0m`,
-        browser: `color: black; background-color: white;`
-    },
+    default: isNode ? `\u001b[49m\u001b[0m` : `color: black; background-color: white;`,
 
     // Colors
-    black:         {cli: `\u001b[30m`, browser: `color: #333`                 },
-    blue:          {cli: `\u001b[34m`, browser: `color: #3498db`              },
-    cyan:          {cli: `\u001b[36m`, browser: `color: #00FFFF`              },
-    gray:          {cli: `\u001b[90m`, browser: `color: #7f8c8d`              },
-    grey:          {cli: `\u001b[90m`, browser: `color: #7f8c8d`              },
-    green:         {cli: `\u001b[32m`, browser: `color: #2ecc71`              },
-    magenta:       {cli: `\u001b[35m`, browser: `color: #8e44ad`              },
-    red:           {cli: `\u001b[31m`, browser: `color: #e74c3c`              },
-    white:         {cli: `\u001b[37m`, browser: `color: #fff`                 },
-    yellow:        {cli: `\u001b[33m`, browser: `color: #f1c40f`              },
+    black:         isNode ? `\u001b[30m` : `color: #333`,
+    blue:          isNode ? `\u001b[34m` : `color: #3498db`,
+    cyan:          isNode ? `\u001b[36m` : `color: #00FFFF`,
+    gray:          isNode ? `\u001b[90m` : `color: #7f8c8d`,
+    grey:          isNode ? `\u001b[90m` : `color: #7f8c8d`,
+    green:         isNode ? `\u001b[32m` : `color: #2ecc71`,
+    magenta:       isNode ? `\u001b[35m` : `color: #8e44ad`,
+    red:           isNode ? `\u001b[31m` : `color: #e74c3c`,
+    white:         isNode ? `\u001b[37m` : `color: #fff`,
+    yellow:        isNode ? `\u001b[33m` : `color: #f1c40f`,
 
     // Background colors
-    bgBlack:       {cli: `\u001b[40m`, browser: `background-color: #333`      },
-    bgBlue:        {cli: `\u001b[44m`, browser: `background-color: #3498db`   },
-    bgCyan:        {cli: `\u001b[46m`, browser: `background-color: #00ffff`   },
-    bgGray:        {cli: `\u001b[47m`, browser: `background-color: #7f8c8d`   },
-    bgGrey:        {cli: `\u001b[47m`, browser: `background-color: #7f8c8d`   },
-    bgGreen:       {cli: `\u001b[42m`, browser: `background-color: #2ecc71`   },
-    bgMagenta:     {cli: `\u001b[45m`, browser: `background-color: #8e44ad`   },
-    bgRed:         {cli: `\u001b[41m`, browser: `background-color: #e74c3c`   },
-    bgWhite:       {cli: `\u001b[47m`, browser: `background-color: #fff`      },
-    bgYellow:      {cli: `\u001b[43m`, browser: `background-color: #f1c40f`   },
+    bgBlack:       isNode ? `\u001b[40m` : `background-color: #333`,
+    bgBlue:        isNode ? `\u001b[44m` : `background-color: #3498db`,
+    bgCyan:        isNode ? `\u001b[46m` : `background-color: #00ffff`,
+    bgGray:        isNode ? `\u001b[47m` : `background-color: #7f8c8d`,
+    bgGrey:        isNode ? `\u001b[47m` : `background-color: #7f8c8d`,
+    bgGreen:       isNode ? `\u001b[42m` : `background-color: #2ecc71`,
+    bgMagenta:     isNode ? `\u001b[45m` : `background-color: #8e44ad`,
+    bgRed:         isNode ? `\u001b[41m` : `background-color: #e74c3c`,
+    bgWhite:       isNode ? `\u001b[47m` : `background-color: #fff`,
+    bgYellow:      isNode ? `\u001b[43m` : `background-color: #f1c40f`,
 
     // Misc style settings
-    blink:         {cli: `\u001b[5m`, browser: `text-decoration: blink`       },
-    bold:          {cli: `\u001b[1m`, browser: `font-weight: bold`            },
-    dim:           {cli: `\u001b[2m`, browser: `opacity: .8`                  },
-    hidden:        {cli: `\u001b[8m`, browser: `visibility: hidden`           },
-    inverse:       {cli: `\u001b[7m`, browser: cssInverse                     },
-    italic:        {cli: `\u001b[3m`, browser: `font-style: italic`           },
-    strikethrough: {cli: `\u001b[9m`, browser: `text-decoration: line-through`},
-    underline:     {cli: `\u001b[4m`, browser: `text-decoration: underline`   },
+    blink:         isNode ? `\u001b[5m` : `text-decoration: blink`,
+    bold:          isNode ? `\u001b[1m` : `font-weight: bold`,
+    dim:           isNode ? `\u001b[2m` : `opacity: .8`,
+    hidden:        isNode ? `\u001b[8m` : `visibility: hidden`,
+    inverse:       isNode ? `\u001b[7m` : cssInverse ,
+    italic:        isNode ? `\u001b[3m` : `font-style: italic`,
+    strikethrough: isNode ? `\u001b[9m` : `text-decoration: line-through`,
+    underline:     isNode ? `\u001b[4m` : `text-decoration: underline`,
 };
